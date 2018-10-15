ee5b14dd9d70701ccc965d9f5d18924a9621da43
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-15 21:34:51 +0900

    Add gesture on Object

--- ARKitExample/ARKitExample/Box/SimpleBoxViewController.swift
+++ ARKitExample/ARKitExample/Box/SimpleBoxViewController.swift
@@ -28,9 +28,12 @@ class SimpleBoxViewController: UIViewController {
         
         let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
         let material = generateMaterial(background: .red)
+        material.name = "color"
         let node = generateNode(with: box, [material], at: SCNVector3(0, 0.1, -0.5))
         let scene = generateScene(with: [node])
         self.sceneView.scene = scene
+        
+        addGesture(on: self.sceneView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -69,5 +72,23 @@ class SimpleBoxViewController: UIViewController {
         let material = SCNMaterial()
         material.diffuse.contents = background
         return material
+    }
+    
+    private func addGesture(on view: UIView) {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapped)))
+    }
+    
+    //MARK: Action
+    @objc func handleTapped(_ gesture: UITapGestureRecognizer) {
+        guard let sceneView = gesture.view as? ARSCNView else { return }
+        let location = gesture.location(in: sceneView)
+        let results = sceneView.hitTest(location, options: [:])
+        
+        if !results.isEmpty {
+            let node = results[0].node
+            let material = node.geometry?.material(named: "color")
+            let color = material?.diffuse.contents as? UIColor
+            material?.diffuse.contents = color == UIColor.red ? UIColor.black : UIColor.red
+        }
     }
 }
